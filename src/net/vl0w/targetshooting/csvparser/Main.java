89a8bdb3551dc962bea668518663c0cc5d8e0ffc
Jonas Hansen <jonas.hansen@bluewin.ch>
2015-05-28 10:40:02 +0200

    Use apache commons-cli as args parser

--- src/net/vl0w/targetshooting/csvparser/Main.java
+++ src/net/vl0w/targetshooting/csvparser/Main.java
@@ -2,17 +2,36 @@ package net.vl0w.targetshooting.csvparser;
 
 import java.util.Collection;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
 import com.google.gson.Gson;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		if (args.length < 2) {
-			throw new IllegalArgumentException(
-					"You must provide a file path in the first argument and a shooter id in the second argument");
-		}
+		Options options = new Options();
+		options.addOption(Option.builder("path") //
+				.required() //
+				.desc("Path to the CSV file") //
+				.hasArg().argName("path") //
+				.build());
 
-		CsvParser parser = new CsvParser(args[0], args[1]);
-		Collection<Event> events = parser.parse();
+		options.addOption(Option.builder("shooter") //
+				.required() //
+				.desc("ID of the shooter") //
+				.hasArg().argName("shooter") //
+				.build());
+
+		CommandLineParser parser = new DefaultParser();
+		CommandLine line = parser.parse(options, args);
+		String csvPath = line.getOptionValue("path");
+		String shooterId = line.getOptionValue("shooter");
+
+		CsvParser csvParser = new CsvParser(csvPath, shooterId);
+		Collection<Event> events = csvParser.parse();
 
 		Gson gson = new Gson();
 		System.out.println(gson.toJson(events));
